629052967e9325ff303d3ea73f24d05ae3124f65
elijahcraig45 <elijahcraig45@gmail.com>
2024-03-25 20:22:30 -0400

    Update to add cors

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const { Pool } = require('pg');
-
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(cors()); // This will enable CORS for all routes and origins
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
