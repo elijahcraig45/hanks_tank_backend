886112a2638edf54c23639e99e74b0097067cb5e
elijahcraig45 <elijahcraig45@gmail.com>
2024-03-30 19:26:30 -0400

    Add Player endpoint to backend

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
+const { exec } = require('child_process');
 const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -125,6 +126,69 @@ app.get('/api/teamData', async (req, res) => {
       res.status(500).send('Internal Server Error');
   }
 });
+
+const pythonEnvPath = '/home/henrycraig/csvkit_env/bin/python3';
+
+app.get('/player-stats/:firstName/:lastName', async (req, res) => {
+  const { firstName, lastName } = req.params;
+
+  // Use the Python executable from the virtual environment
+  const scriptPath = '/home/henrycraig/mlb_pi/getPlayerID.py'; // Make sure to use the correct path
+  exec(`${pythonEnvPath} ${scriptPath} "${firstName}" "${lastName}"`, async (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return res.status(500).send('Internal Server Error');
+    }
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return res.status(404).send('Player not found');
+    }
+    const playerId = stdout.trim();
+    try {
+      // Query your database using the player ID
+      // This is a placeholder query; modify it according to your schema
+      const statcast_batting = await pool.query('SELECT * FROM "statcast_2024" WHERE "batter" = $1', [playerId]);
+      const statcast_pitching = await pool.query('SELECT * FROM "statcast_2024" WHERE "pitcher" = $1', [playerId]);
+      const playerBatting_2024 = await pool.query(`SELECT * FROM "playerBatting_2024" WHERE "Name" = \'${firstName} ${lastName}\'`);
+      const playerPitching_2024 = await pool.query(`SELECT * FROM "playerPitching_2024" WHERE "Name" = \'${firstName} ${lastName}\'`);
+      const playerBatting_2023 = await pool.query(`SELECT * FROM "playerBatting_2023" WHERE "Name" = \'${firstName} ${lastName}\'`);
+      const playerPitching_2023 = await pool.query(`SELECT * FROM "playerPitching_2023" WHERE "Name" = \'${firstName} ${lastName}\'`);
+      const playerBatting_2022 = await pool.query(`SELECT * FROM "playerBatting_2022" WHERE "Name" = \'${firstName} ${lastName}\'`);
+      const playerPitching_2022 = await pool.query(`SELECT * FROM "playerPitching_2022" WHERE "Name" = \'${firstName} ${lastName}\'`);
+      const playerBatting_2021 = await pool.query(`SELECT * FROM "playerBatting_2021" WHERE "Name" = \'${firstName} ${lastName}\'`);
+      const playerPitching_2021 = await pool.query(`SELECT * FROM "playerPitching_2021" WHERE "Name" = \'${firstName} ${lastName}\'`);
+      const playerBatting_2020 = await pool.query(`SELECT * FROM "playerBatting_2020" WHERE "Name" = \'${firstName} ${lastName}\'`);
+      const playerPitching_2020 = await pool.query(`SELECT * FROM "playerPitching_2020" WHERE "Name" = \'${firstName} ${lastName}\'`);
+      const playerBatting_2019 = await pool.query(`SELECT * FROM "playerBatting_2019" WHERE "Name" = \'${firstName} ${lastName}\'`);
+      const playerPitching_2019 = await pool.query(`SELECT * FROM "playerPitching_2019" WHERE "Name" = \'${firstName} ${lastName}\'`);
+
+
+        // Aggregate data into one response
+  const responseData = {
+    playerBatting_2024: playerBatting_2024.rows,
+    playerPitching_2024: playerPitching_2024.rows,
+    playerBatting_2023: playerBatting_2023.rows,
+    playerPitching_2023: playerPitching_2023.rows,
+    playerBatting_2022: playerBatting_2022.rows,
+    playerPitching_2022: playerPitching_2022.rows,
+    playerBatting_2021: playerBatting_2021.rows,
+    playerPitching_2021: playerPitching_2021.rows,
+    playerBatting_2020: playerBatting_2020.rows,
+    playerPitching_2020: playerPitching_2020.rows,
+    playerBatting_2019: playerBatting_2019.rows,
+    playerPitching_2019: playerPitching_2019.rows,
+    statcastBatting: statcast_batting.rows,
+    statcastPitching: statcast_pitching.rows
+};
+
+res.json(responseData);
+    } catch (dbError) {
+      console.error(`Database error: ${dbError}`);
+      res.status(500).send('Database query failed');
+    }
+  });
+});
+
 
 
 // MLB News endpoint
